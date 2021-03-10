580a5e2de34e47eacea42d9869f9a96cf293f933
Jason Dong <dongpeijian@innotechx.com>
2021-03-10 22:25:59 +0800

    update OK

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   version: "1.0.0",
   note: '初版出道', // 这个为版本描述，无需修改
-  shareProfile: '打造智慧城市', // 首页转发的时候话术
+  shareProfile: '无线温度检测', // 首页转发的时候话术
   goodsDetailSkuShowType: 0, // 0 为点击立即购买按钮后出现规格尺寸、数量的选择； 1为直接在商品详情页面显示规格尺寸、数量的选择，而不弹框
   shopMod: 0, // 0为单店铺版本 ； 1为多店铺版本
   subDomain: 'smartcity',
